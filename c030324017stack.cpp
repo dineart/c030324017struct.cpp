aab0a5bccb2db5b30a17933dfe1d5fa336648775
dineart <di2601na@gmail.com>
2025-05-28 12:32:00 +0800

    Menambah file stack

---
+++ c030324017stack.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+using namespace std;
+
+#define MAXSTACK 100
+
+typedef int ItemType;
+
+
+typedef struct{
+    ItemType Item[MAXSTACK];
+    int Count;
+}Stack;
+
+
+
+void InitializeStack(Stack *S){
+    S->Count = 0;
+}
+
+int Empty(Stack *S){
+    return (S->Count == 0);
+}
+
+int Full(Stack *S){
+    return (S->Count == MAXSTACK);
+}
+void Pop(Stack *S, ItemType *x){
+    if (Empty(S)){
+        cout<<"Stack masih kosong!"<<endl;
+    }
+    else{
+        --(S->Count);
+        *x = S->Item[S->Count];
+
+    }
+}
+
+void Push(ItemType x, Stack *S)
+{
+    if (Full(S)){
+        cout << "Stack penuh! Data tidak dapat masuk!" <<endl;
+    }
+    else{
+        S->Item[S->Count]=x;
+        ++(S->Count);
+    }
+}
+
+void PrintStack(Stack *S) {
+    cout << "Isi stack:";
+    for (int i = 0; i < S->Count; i++) 
+        cout << S->Item[i] << " ";
+    cout << endl;
+
+}
+
+
+int main() {
+    Stack S;
+    ItemType x;
+    InitializeStack(&S);
+
+    Push(10, &S);
+    Push(20, &S);
+    Push(30, &S);
+
+    PrintStack(&S);
+
+    Pop(&S, &x);
+    cout << "Item yang di-pop:" << x << endl;
+
+    PrintStack(&S);
+
+    
+    return 0;
+}
